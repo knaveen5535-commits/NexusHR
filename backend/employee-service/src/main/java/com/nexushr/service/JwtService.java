f453a23b4073d52690a4f635f857f5f136c9e49e
EdgeWay Agency <deepcoders0@gmail.com>
2026-06-27 23:22:42 +0530

    Completed employee service with CRUD validation and role based security

---
+++ backend/employee-service/src/main/java/com/nexushr/service/JwtService.java
@@ -0,0 +1,27 @@
+package com.nexushr.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
+
+    public Claims extractClaims(String token){
+
+        SecretKey key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+}
